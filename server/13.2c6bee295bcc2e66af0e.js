48aa294abf1b0c8cff94227df64d330d6caf4d57
dingjianz <573828395@qq.com>
2020-11-28 22:44:27 +0800

    init

---
+++ server/13.2c6bee295bcc2e66af0e.js
@@ -0,0 +1,26 @@
+exports.ids = [13];
+exports.modules = {
+
+/***/ "XpxN":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("F5FC");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("cDcd");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+const DynamicComponnet = () => {
+  return /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__["jsx"])("div", {
+    children: "DynamicComponnet"
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (DynamicComponnet);
+
+/***/ })
+
+};;
---
+++ server/14.a9f4f2a4755beece90db.js
@@ -0,0 +1,34 @@
+exports.ids = [14];
+exports.modules = {
+
+/***/ "tuEw":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "fn1", function() { return fn1; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "fn2", function() { return fn2; });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("F5FC");
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("cDcd");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+const fn1 = () => {
+  return /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__["jsx"])("div", {
+    children: "fn1"
+  });
+};
+
+const fn2 = () => {
+  return /*#__PURE__*/Object(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__["jsx"])("div", {
+    children: "fn2"
+  });
+};
+
+
+
+/***/ })
+
+};;
